3828be2069597d8664f2f091357ebec9d9f3f47a
XniceCraft <xnicecraftacc@gmail.com>
2025-12-11 12:40:31 +0700

    fix: request soft delete
    feat: add cancel request and next action request

--- src/api/request/controllers/request.ts
+++ src/api/request/controllers/request.ts
@@ -10,14 +10,14 @@ export default factories.createCoreController(
         async softDelete(ctx) {
             await this.validateQuery(ctx);
 
-            const { slug } = ctx.params;
-            if (!slug) return ctx.notFound("Slug not defined");
+            const { id } = ctx.params;
+            if (!id) return ctx.notFound("Slug not defined");
 
             const request = await strapi
                 .documents("api::request.request")
-                .findFirst({
+                .findOne({
+                    documentId: id,
                     status: "published",
-                    where: { slug },
                 });
 
             if (!request) return ctx.notFound("Request not found");
@@ -31,6 +31,118 @@ export default factories.createCoreController(
             ctx.status = 204;
             const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
             return await this.transformResponse(sanitizedEntity);
+        },
+
+        async nextAction(ctx) {
+            await this.validateQuery(ctx);
+            try {
+                const { id } = ctx.params;
+                const request = await strapi
+                    .documents("api::request.request")
+                    .findOne({
+                        documentId: id,
+                    });
+
+                if (!request) {
+                    return ctx.notFound("Order not found");
+                }
+
+                switch (request.requestStatus) {
+                    case "cancelled":
+                        return ctx.badRequest("Order is already cancelled");
+                    case "completed":
+                        return ctx.badRequest("Order is already completed");
+                }
+
+                let nextRequestStatus:
+                    | "pending"
+                    | "confirmed"
+                    | "in_progress"
+                    | "completed"
+                    | "cancelled" = "pending";
+
+                switch (request.requestStatus) {
+                    case "pending":
+                        nextRequestStatus = "confirmed";
+                        break;
+                    case "confirmed":
+                        nextRequestStatus = "in_progress";
+                        break;
+                    case "in_progress":
+                        nextRequestStatus = "completed";
+                        break;
+                }
+
+                const updatedOrder = await strapi
+                    .documents("api::request.request")
+                    .update({
+                        documentId: id,
+                        data: {
+                            requestStatus: nextRequestStatus,
+                        },
+                    });
+
+                const sanitizedEntity = await this.sanitizeOutput(
+                    updatedOrder,
+                    ctx
+                );
+
+                return await this.transformResponse(sanitizedEntity, null);
+            } catch (error) {
+                strapi.log.error("Order next action failed:", error);
+                return ctx.internalServerError("Failed to update order");
+            }
+        },
+
+        async cancelOrder(ctx) {
+            try {
+                const { id } = ctx.params;
+                const request = await strapi
+                    .documents("api::request.request")
+                    .findOne({
+                        documentId: id,
+                    });
+
+                if (!request) {
+                    return ctx.notFound("Order not found");
+                }
+
+                switch (request.requestStatus) {
+                    case "cancelled":
+                        return ctx.badRequest("Order is already cancelled");
+                    case "completed":
+                        return ctx.badRequest("Order is already completed");
+                    case "in_progress":
+                        return ctx.badRequest(
+                            "Order is already in progress. Please contact our support"
+                        );
+                }
+
+                const cancelledRequest = await strapi
+                    .documents("api::request.request")
+                    .update({
+                        documentId: id,
+                        data: {
+                            requestStatus: "cancelled",
+                        },
+                    });
+
+                const sanitizedEntity = await this.sanitizeOutput(
+                    cancelledRequest,
+                    ctx
+                );
+
+                const response = await this.transformResponse(
+                    sanitizedEntity,
+                    null
+                );
+                response["message"] = "Successfully cancelled order";
+
+                return response;
+            } catch (error) {
+                strapi.log.error("Order cancel failed:", error);
+                return ctx.internalServerError("Failed to cancel order");
+            }
         },
     })
 );
--- src/api/request/routes/custom-route.ts
+++ src/api/request/routes/custom-route.ts
@@ -2,8 +2,18 @@ export default {
     routes: [
         {
             method: "DELETE",
-            path: "/requests/:slug/soft-delete",
+            path: "/requests/:id/soft-delete",
             handler: "request.softDelete",
+        },
+        {
+            method: "POST",
+            path: "/requests/:id/cancel",
+            handler: "request.cancelOrder",
+        },
+        {
+            method: "POST",
+            path: "/requests/:id/next-action",
+            handler: "request.nextAction",
         },
     ],
 };
